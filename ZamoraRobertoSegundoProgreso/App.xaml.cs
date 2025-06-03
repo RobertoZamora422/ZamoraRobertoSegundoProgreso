769ac9bb22347e2862a05c889de53ba0f126e79e
laboratorio <laboratorio@UPOAULA10708>
2025-06-03 07:20:26 -0500

    Agregar archivos de proyecto.

---
+++ ZamoraRobertoSegundoProgreso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZamoraRobertoSegundoProgreso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
